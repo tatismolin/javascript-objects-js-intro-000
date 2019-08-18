3bd1a77b0119b432b715f3fbd6d71917f9b3b1b3
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-18 21:53:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,15 @@
 var playlist = {
-  name: 'song'
+  artistName: 'songTitle'
 }
 
-function updatePlaylist(playlist, name, song){
+function updatePlaylist(playlist, artistName, songTitle){
+  Object.assign({}, {Slowdive: 'song1'})
+  Object.assign({}, {My Bloody Valentine: 'song1'})
+  Object.assign({}, {Phil Ochs: 'song1'})
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName){
   Object.assign({}, {name1: 'song1'})
   return playlist;
 }
