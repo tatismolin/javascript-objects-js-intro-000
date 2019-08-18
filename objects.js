e3f2dad6a4077621026a57a22950363790f9ca05
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-18 22:01:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({}, {Slowdive: 'song1'})
+  Object.assign({playlist}, {Slowdive: 'song1'})
   return playlist;
 }
 
