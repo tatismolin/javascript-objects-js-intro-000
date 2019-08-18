1f6f09c72ad1061dc5f1051cdf05c07e2e0549fa
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-18 22:15:49 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,6 +10,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete 'Slowdive'.songTitle;
+  delete playlist."Slowdive";
   return playlist;
 }
